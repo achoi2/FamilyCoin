c500f3368c4b2609dc7420e484c6031e2a8525ef
Andrew <andrewchoi1@hotmail.com>
2018-10-15 14:13:04 -0400

    completed routing

--- src/Components/ViewTask.js
+++ src/Components/ViewTask.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import web3 from '../web3';
 import Allowance from '../getallowance';
 import CreateTaskForm from './CreateTaskForm';
+import TasksTable from './TasksTable';
 
 class ViewTask extends Component {
     constructor(props) {
@@ -9,53 +10,71 @@ class ViewTask extends Component {
         this.state = {
             task: [],
             parent: '',
-            value: ''
+            value: '',
+            taskInput: '',
+            valueInput: ''
         };
     }
 
     async componentDidMount() {
-        const allowance = Allowance(this.props.acNum);
+        const allowance = Allowance(this.props.match.params.acNum);
         const summary = await allowance.methods.getSummary().call();
-        const taskCount = await allowance.methods.getTaskCount().call()
+        const taskCount = await allowance.methods.getTaskCount().call();
+
 
         const task = await Promise.all(
-            Array(parseInt(taskCount)).fill().map((element, index) => {
-                return allowance.methods.tasks(index).call()
-            })
+            Array(parseInt(taskCount))
+                .fill()
+                .map((element, index) => {
+                    return allowance.methods.tasks(index).call();
+                })
         );
-
 
         this.setState({
             tasks: summary[0],
             parent: summary[1],
-            task
+            task: task
         });
     }
 
     createTask = async e => {
         e.preventDefault();
-        const allowance = Allowance(this.props.acNum);
+        const allowance = Allowance(this.props.match.params.acNum);
         try {
             const accounts = await web3.eth.getAccounts();
-            await allowance.methods.createTask(this.state.task, this.state.value).send({
-                from: accounts[0],
-                value: web3.utils.toWei(this.state.value, 'ether')
-            });
-        } catch (err) {
-
-        }
+            await allowance.methods
+                .createTask(this.state.taskInput, this.state.valueInput)
+                .send({
+                    from: accounts[0],
+                    value: web3.utils.toWei(this.state.valueInput, 'ether')
+                });
+        } catch (err) {}
     };
 
     render() {
+        const handleChangeTask = (task) => {
+            this.setState({ taskInput: task });
+        };
+
+        const handleChangeValue = (value) => {
+            this.setState({ valueInput: value });
+        }
+        
         return (
             <div>
                 <h3>Parent Account: {this.state.parent}</h3>
                 <h4>There are {this.state.tasks} tasks</h4>
-                <CreateTaskForm  createTask={this.createTask} value={this.state.value}/>
+                <CreateTaskForm
+                    handleChangeTask={handleChangeTask}
+                    handleChangeValue={handleChangeValue}
+                    createTask={this.createTask}
+                    value={this.state.valueInput}
+                    task={this.state.taskInput}
+                />
+                <TasksTable task={this.state.task}/>
             </div>
         );
     }
 }
-
 
 export default ViewTask;
--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './Components/HomePage';
+// import FamilyAccount from './Components/FamilyAccount';
+import ViewTask from './Components/ViewTask';
+
 const Router = () => {
     return (
         <HashRouter>
             <div>
                 <Switch>
                     <Route exact path="/" component={HomePage} />
+                    <Route path="/:acNum" component={ViewTask}/>
                 </Switch>
             </div>
         </HashRouter>
